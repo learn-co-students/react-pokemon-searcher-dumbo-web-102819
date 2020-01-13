034868aaf1b2fb89e7085a6c1fc1878d90fd3517
Griffin Poole <griffinpoole5@gmail.com>
2020-01-13 17:54:33 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,17 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+  renderPokemon = () => {
+    return this.props.pokemonArr.map((pokemon, i) => {
+      return (<PokemonCard key={i} pokemon={pokemon}/>)
+    })
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.renderPokemon()}
       </Card.Group>
     )
   }
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -6,11 +6,38 @@ class PokemonForm extends React.Component {
     super()
 
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
+      id: 1,
+      name: 'bulbasaur',
+      hp: 39,
+      frontUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      backUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png'
     }
+  }
+
+  handleSubmit = (event) => {
+    let newPokemon = {
+      "id": parseInt(this.state.id),
+      "name": this.state.name,
+      "stats": [
+        {
+          "value": parseInt(this.state.hp),
+          "name": "hp"
+        }
+      ],
+      "types": ["grass", "poison"],
+      "sprites": {
+        "front": this.state.frontUrl,
+        "back": this.state.backUrl
+      }
+    }
+    this.props.addPokemon(newPokemon)
+  }
+
+  handleChange = (event) => {
+    let newState = {
+      [event.target.name]: event.target.value
+    }
+    this.setState(newState)
   }
 
   render() {
@@ -19,10 +46,11 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid onChange={this.handleChange} type="number" value={this.state.id} label="id" placeholder="id" name="id" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.name} label="Name" placeholder="Name" name="name" />
+            <Form.Input fluid onChange={this.handleChange} type="number" value={this.state.hp} label="hp" placeholder="hp" name="hp" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.frontUrl} label="Front Image URL" placeholder="url" name="frontUrl" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.backUrl} label="Back Image URL" placeholder="url" name="backUrl" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
